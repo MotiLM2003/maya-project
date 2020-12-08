77ee9b28a28402f5a5b9aec85842a790c3bde020
Moti Elmakies <motiphone2003@gmail.com>
2020-12-09 00:42:45 +0200

    :smile: added happy emojies

--- sitejs.js
+++ sitejs.js
@@ -1,7 +1,5 @@
 const button = document.getElementById('btn-change-image');
 const image = document.getElementById('myPic');
-const happyEmojy = document.getElementById('happy-emojy');
-const buttonText = document.getElementById('button-text');
 button.addEventListener('click', changeImage);
 
 function changeImage() {
@@ -9,13 +7,13 @@ function changeImage() {
     image.classList.remove('box-out');
     image.classList.add('box-in');
     image.src = 'maya_happy.jpeg';
-    happyEmojy.src = 'happier.png';
-    buttonText.textContent = ' Make Maya Happy';
+
+    button.textContent = '🙂 Make Maya Happy   🙂';
   } else {
     image.classList.remove('box-in');
     image.classList.add('box-out');
     image.src = 'maya_picture.jpeg';
-    happyEmojy.src = 'happy.png';
-    buttonText.textContent = ' Make Maya Happier';
+
+    button.textContent = '😁 Make Maya Happier 😁';
   }
 }
